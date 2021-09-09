366ffcbe480ae7815ecb890f7ff38bdd4918532e
Bhishmakvij <79918644+Bhishmakvij@users.noreply.github.com>
2021-09-09 19:38:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Family Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
-var i = 0;
+menu_list_array = [ "Veg Margherita Pizza", "Mexican Green Wave", "Peppy Paneer", "Deluxe Veggie", "Indi Tandoori Paneer" ,"PANEER MAKHANI" ];
 
-function update()
-{
-    i++;
-    var numbers_of_family_member_in_array = 5
-    if(i > numbers_of_family_member_in_array)
-      {
-          i = 0;
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
 
+}
+
+function add_item(){
+var htmldata;
+var item = document.getElementById("add_item").value;
+
+menu_list_array.sort();
+
+
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
       }
-    
 
-    var updatedImage = images[i];
-
-    var updatedName = names[i];
- 
-    document.getElementById("family_member_image").src = updatedImage;
-   
-    document.getElementById("family_member_name").innerHTML = updatedName;
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
 }
